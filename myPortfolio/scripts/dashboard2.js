4a1d7f0df9f7a3812ffab213563448e67a7790e1
ck1962 <ckeslin@gmail.com>
2019-04-01 14:21:03 -0500

    fixed to do

--- myPortfolio/scripts/dashboard2.js
+++ myPortfolio/scripts/dashboard2.js
@@ -12,6 +12,24 @@ function displayPosition(position) {
   myLocation.innerHTML = "Longitude: " + position.coords.longitude + " Latitude: " + position.coords.latitude
 }
 
+$(function() {
+
+  let $list = $('ul');
+  let $newItemForm = $('#newItemForm');
+
+  $newItemForm.on('submit', function(e) {
+    e.preventDefault();
+    let text = $('input[type="text"]').val();
+    $list.append(`<li>${text}</li>`);
+    $('input[type="text"]').val('');
+  });
+
+  $list.on('click', 'li', function() {
+    let $this = $(this);
+    $this.remove();
+  });
+
+});
 
 
 
