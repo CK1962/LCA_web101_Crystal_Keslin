a5fcf43a9044fe04ba9883ffa1c37b212266c748
ck1962 <ckeslin@gmail.com>
2019-04-03 15:31:12 -0500

    clean up code

--- myPortfolio/scripts/dashboard2.js
+++ myPortfolio/scripts/dashboard2.js
@@ -1,127 +1,143 @@
+// Geo location
 var myLocation = document.getElementById("my-coordinates")
 
 function getLocation() {
-  if(navigator.geolocation){
+  if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(displayPosition)
   } else {
-   myLocation.innerHTML = "Not available"
+    myLocation.innerHTML = "Not available"
   }
 }
 
+// exact coordinates
 function displayPosition(position) {
   myLocation.innerHTML = "Longitude: " + position.coords.longitude + " Latitude: " + position.coords.latitude
 }
 
-$(function() {
-
+// add new ToDo items
+$(function () {
   let $list = $('ul');
   let $newItemForm = $('#newItemForm');
 
-  $newItemForm.on('submit', function(e) {
+  $newItemForm.on('submit', function (e) {
     e.preventDefault();
     let text = $('input[type="text"]').val();
     $list.append(`<li>${text}</li>`);
     $('input[type="text"]').val('');
   });
 
-  $list.on('click', 'li', function() {
+  $list.on('click', 'li', function () {
     let $this = $(this);
     $this.remove();
   });
 
 });
 
-$("#wrapper").click('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-});
+// My click destresser
+function animationClick(element, animation) {
+  element = $(element);
+  element.click(
+    function () {
+      element.addClass('animated ' + animation);
+      //wait for animation to finish before removing classes
+      window.setTimeout(function () {
+        element.removeClass('animated ' + animation);
+      }, 2000);
+    }
+  );
+};
 
+// calendar header
 let _ = selector => {
-    return document.querySelector(selector);
-  };
-  let __ = selector => {
-    return document.querySelectorAll(selector);
-  };
+  return document.querySelector(selector);
+};
+let __ = selector => {
+  return document.querySelectorAll(selector);
+};
 
-  let drawCalendar = dateObj => {
-    _(".calendar-header").innerHTML = "";
-    _(".calendar-body").innerHTML = "";
+let drawCalendar = dateObj => {
+  _(".calendar-header").innerHTML = "";
+  _(".calendar-body").innerHTML = "";
 
-    let dateInstance = dateObj || new Date();
+  let dateInstance = dateObj || new Date();
 
-    let dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+// days of the week
+  let dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-    let date = dateInstance.getDate();
+  let date = dateInstance.getDate();
 
-    let day = dateInstance.getDay();
+  let day = dateInstance.getDay();
 
-    let dayName = dayNames[day];
+  let dayName = dayNames[day];
 
-    let month = dateInstance.getMonth();
+  let month = dateInstance.getMonth();
 
-    let monthNames = [
-      "Jan",
-      "Feb",
-      "Mar",
-      "Apr",
-      "May",
-      "Jun",
-      "Jul",
-      "Aug",
-      "Sep",
-      "Oct",
-      "Nov",
-      "Dec"
-    ];
+  // months of the year
+  let monthNames = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+  ];
 
-    let year = dateInstance.getFullYear();
+  let year = dateInstance.getFullYear();
 
-    let daysInAMonth = new Date(year, month + 1, 0).getDate();
+  let daysInAMonth = new Date(year, month + 1, 0).getDate();
 
-    let indexOfTheStartDay = new Date(year, month, 1).getDay();
+  let indexOfTheStartDay = new Date(year, month, 1).getDay();
 
-    let header = _(".calendar-header");
-    let headerTemplate = "";
-    dayNames.forEach(function(i) {
-      headerTemplate += `
+  let header = _(".calendar-header");
+  let headerTemplate = "";
+  dayNames.forEach(function (i) {
+    headerTemplate += `
       <div class="day-name">${i}</div>
     `;
-    });
+  });
 
     header.innerHTML = headerTemplate;
-    let body = _(".calendar-body");
+  let body = _(".calendar-body");
 
-    let offsetTemplate = `<div class="day"></div>`.repeat(indexOfTheStartDay);
-    let bodyTemplate = offsetTemplate;
+  let offsetTemplate = `<div class="day"></div>`.repeat(indexOfTheStartDay);
+  let bodyTemplate = offsetTemplate;
 
-    for (let i = 1; i <= daysInAMonth; i++) {
-      bodyTemplate += `<div class="day">${i}</div>`;
-    }
-    body.innerHTML = bodyTemplate;
+  for (let i = 1; i <= daysInAMonth; i++) {
+    bodyTemplate += `<div class="day">${i}</div>`;
+  }
+  body.innerHTML = bodyTemplate;
 
-    let monthsTemplate = "";
-    for (let i = 0; i < monthNames.length; i++) {
-      let property = i == month ? "selected" : "";
-      monthsTemplate += `<option value="${i}" ${property}>${monthNames[
+  let monthsTemplate = "";
+  for (let i = 0; i < monthNames.length; i++) {
+    let property = i == month ? "selected" : "";
+    monthsTemplate += `<option value="${i}" ${property}>${monthNames[
         i
       ]}</option>`;
-    }
-    _("#months").innerHTML = monthsTemplate;
+  }
+  _("#months").innerHTML = monthsTemplate;
 
-    let yearTemplate = "";
-    for (let i = new Date().getFullYear(); i > 1990; i--) {
-      let property = i == year ? "selected" : "";
-      yearTemplate += `<option value="${i}" ${property}>${i}</option>`;
-    }
+  let yearTemplate = "";
+  for (let i = new Date().getFullYear(); i > 1990; i--) {
+    let property = i == year ? "selected" : "";
+    yearTemplate += `<option value="${i}" ${property}>${i}</option>`;
+  }
 
-    _("#years").innerHTML = yearTemplate;
-  };
+  _("#years").innerHTML = yearTemplate;
+};
 
-  drawCalendar();
+drawCalendar();
 
-  __("#years, #months").forEach(function(ref) {
-    ref.addEventListener("change", function() {
-      let dateInstance = new Date();
-      let year = _("#years").value || dateInstance.getFullYear();
-      let month = _("#months").value || dateInstance.getMonth();
-      drawCalendar(new Date(year, month));
-    });
+__("#years, #months").forEach(function (ref) {
+  ref.addEventListener("change", function () {
+    let dateInstance = new Date();
+    let year = _("#years").value || dateInstance.getFullYear();
+    let month = _("#months").value || dateInstance.getMonth();
+    drawCalendar(new Date(year, month));
   });
+});
